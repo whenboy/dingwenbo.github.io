7103e9a0df1c174c025bb1f6f96acb805f61759e
whenboy <76686147+whenboy@users.noreply.github.com>
2020-12-28 21:23:22 +0800

    Init
    Init

---
+++ Run.bat
@@ -0,0 +1,7 @@
+python jemdoc.py index.jemdoc
+python jemdoc.py publications.jemdoc
+python jemdoc.py experience.jemdoc
+python jemdoc.py honors.jemdoc
+python jemdoc.py others.jemdoc
+python jemdoc.py aboutme.jemdoc
+python jemdoc.py travel.jemdoc
